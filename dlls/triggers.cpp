25b1a81555798b20f6660061f7de3beee38e9062
FreeSlave <freeslave93@gmail.com>
2015-07-07 00:34:45 +0400

    Add trigger_playerfreeze

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -2074,7 +2074,23 @@ void CTriggerGravity::GravityTouch( CBaseEntity *pOther )
 
 
 
+class CTriggerPlayerFreeze : public CBaseDelay
+{
+public:
+	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
+	int ObjectCaps( void ) { return CBaseDelay::ObjectCaps() & ~FCAP_ACROSS_TRANSITION; }
+};
+LINK_ENTITY_TO_CLASS( trigger_playerfreeze, CTriggerPlayerFreeze );
 
+void CTriggerPlayerFreeze::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{
+	if ( !pActivator || !pActivator->IsPlayer() )
+		pActivator = CBaseEntity::Instance(g_engfuncs.pfnPEntityOfEntIndex( 1 ));
+
+		if (pActivator->pev->flags & FL_FROZEN)
+			((CBasePlayer *)((CBaseEntity *)pActivator))->EnableControl(TRUE);
+		else	((CBasePlayer *)((CBaseEntity *)pActivator))->EnableControl(FALSE);
+}
 
 
 
